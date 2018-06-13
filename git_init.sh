09fef661d4177afd1f0fb71f8c04069e7b58e87c
周峰 <zhoufeng@hujiang.com>
2018-06-13 22:10:00 +0800

    Update git info.

--- git_init.sh
+++ git_init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git config --global user.name mritd
-git config --global user.email mritd1234@gmail.com
+git config --global user.name zhoufwind
+git config --global user.email zhoufwind@hotmail.com
 git config --global core.editor vim
 git config --global push.default simple
 git config --global core.excludesfile ~/.global_gitignore
